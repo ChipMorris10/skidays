c7912785fd182052befe13e02f4c6c8692d91f2d
ChipMorris10 <goldcomac@mac.com>
2015-11-28 13:24:29 -0700

    tried to get the ski passes to jiggle

--- client/public/js/directives.js
+++ client/public/js/directives.js
@@ -5,58 +5,5 @@
 //   };
 // });
 
-// app.directive('SOMETHING WILL GO HERE', function() {
-//   return {
-//     restrict: 'E',
-//     templateUrl:"../../../server/views/html_templates/index.html"
-//   };
-// });
 
-// app.directive('SOMETHING WILL GO HERE', function() {
-//   return {
-//     restrict: 'E',
-//     templateUrl:"../../../server/views/html_templates/layout.html"
-//   };
-// });
 
-// app.directive('SOMETHING WILL GO HERE', function() {
-//   return {
-//     restrict: 'E',
-//     templateUrl:"../../../server/views/html_templates/log.html"
-//   };
-// });
-
-// app.directive('SOMETHING WILL GO HERE', function() {
-//   return {
-//     restrict: 'E',
-//     templateUrl:"../../../server/views/html_templates/main_page.html"
-//   };
-// });
-
-// app.directive('SOMETHING WILL GO HERE', function() {
-//   return {
-//     restrict: 'E',
-//     templateUrl:"../../../server/views/html_templates/navbar.html"
-//   };
-// });
-
-// app.directive('SOMETHING WILL GO HERE', function() {
-//   return {
-//     restrict: 'E',
-//     templateUrl:"../../../server/views/html_templates/newtrax.html"
-//   };
-// });
-
-// app.directive('SOMETHING WILL GO HERE', function() {
-//   return {
-//     restrict: 'E',
-//     templateUrl:"../../../server/views/html_templates/photos.html"
-//   };
-// });
-
-// app.directive('SOMETHING WILL GO HERE', function() {
-//   return {
-//     restrict: 'E',
-//     templateUrl:"../../../server/views/html_templates/search.html"
-//   };
-// });
